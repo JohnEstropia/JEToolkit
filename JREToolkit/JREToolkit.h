2d1b2b34f6897ce0e3c371522ef83602daef8318
John Rommel Estropia <john.rommel.estropia@gmail.com>
2013-08-10 20:38:31 +0900

    Initial Commit

---
+++ JREToolkit/JREToolkit.h
@@ -0,0 +1,13 @@
+//
+//  JREToolkit.h
+//  JREToolkit
+//
+//  Created by John Rommel Estropia on 2013/08/10.
+//  Copyright (c) 2013 John Rommel Estropia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JREToolkit : NSObject
+
+@end
---
+++ JREToolkit/JREToolkit.m
@@ -0,0 +1,13 @@
+//
+//  JREToolkit.m
+//  JREToolkit
+//
+//  Created by John Rommel Estropia on 2013/08/10.
+//  Copyright (c) 2013 John Rommel Estropia. All rights reserved.
+//
+
+#import "JREToolkit.h"
+
+@implementation JREToolkit
+
+@end
