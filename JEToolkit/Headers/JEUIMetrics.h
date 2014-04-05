b5df7aa740a852a52655977ef7de678fe26ff8d9
John Rommel Estropia <john.rommel.estropia@gmail.com>
2014-04-05 22:05:38 +0900

    added device metrics

--- JEToolkit/Headers/JEUIMetrics.h
+++ JEToolkit/Headers/JEUIMetrics.h
@@ -10,7 +10,24 @@
 #define JEToolkit_JEUIMetrics_h
 
 
+#pragma mark - Device Metrics
+
+static const CGSize JEUIIPhone4ScreenPortraitSize = (CGSize){ .width = 320.0f, .height = 480.0f };
+static const CGSize JEUIIPhone4ScreenLandscapeSize = (CGSize){ .width = 480.0f, .height = 320.0f };
+
+static const CGSize JEUIIPhone5ScreenPortraitSize = (CGSize){ .width = 320.0f, .height = 568.0f };
+static const CGSize JEUIIPhone5ScreenLandscapeSize = (CGSize){ .width = 568.0f, .height = 320.0f };
+
+static const CGSize JEUIIPadScreenPortraitSize = (CGSize){ .width = 768.0f, .height = 1024.0f };
+static const CGSize JEUIIPadScreenLandscapeSize = (CGSize){ .width = 1024.0f, .height = 768.0f };
+
+
+#pragma mark - Views
+
 static const CGFloat JEUIAdBannerViewHeight = 50.0f;
+static const CGFloat JEUIPickerViewHeight = 216.0f;
+static const CGFloat JEUITextFieldHeight = 31.0f;
+static const CGFloat JEUIToolbarHeight = 44.0f;
 
 static const CGFloat JEUINavigationBarLandscapeHeight = 32.0f;
 static const CGFloat JEUINavigationBarPortraitHeight = 44.0f;
